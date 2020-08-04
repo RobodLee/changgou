883aa4383ccf30f5554775f2770b7d034cb91696
Robod-MI <robodlee@gmail.com>
2020-08-04 13:44:44 +0800

    网关配置

---
+++ changgou-parent/changgou-gateway/changgou-gateway-web/src/main/java/com/robod/GatewayWebApplication.java
@@ -0,0 +1,32 @@
+package com.robod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+/**
+ * @author Robod
+ * @date 2020/8/3 23:41
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayWebApplication.class, args);
+    }
+
+    @Bean(name = "ipKeyResolver")
+    public KeyResolver userKeyResolver() {
+        return exchange -> {
+            String ip = Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getHostName();
+            return Mono.just(ip);
+        };
+    }
+}
